27f4d41d59cde1f2b3577637c91c18592c6d5796
Frank Akrasi Antwi <fantwi@ucc.edu.gh>
2022-09-19 23:53:28 +0000

    Setup and Initial commit

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
 from models import setup_db, Question, Category
+from settings import TEST_DB_NAME, DB_USER, DB_PASS
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -14,8 +15,8 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_name = TEST_DB_NAME
+        self.database_path = "postgresql://{}:{}@{}/{}".format(DB_USER, DB_PASS, 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
