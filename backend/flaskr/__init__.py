d60c70aef0b9abc36c40f24444ab0da2134924a5
Frank Akrasi Antwi <fantwi@ucc.edu.gh>
2022-09-20 13:17:43 +0000

    Added error handler for 400 Bad Request

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -260,6 +260,13 @@ def create_app(test_config=None):
     Create error handlers for all expected errors
     including 404 and 422.
     """
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            'success': False,
+            'error': 400,
+            'message': 'Bad request'
+        }), 400
 
     return app
 
