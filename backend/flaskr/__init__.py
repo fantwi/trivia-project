41916483e757ad8aff4bb5ca00a317d657f33b43
Frank Akrasi Antwi <fantwi@ucc.edu.gh>
2022-09-20 12:51:56 +0000

    Created an endpoint to POST a new question

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -141,6 +141,31 @@ def create_app(test_config=None):
     the form will clear and the question will appear at the end of the last page
     of the questions list in the "List" tab.
     """
+    @app.route('/questions', methods=['POST'])
+    def create_question():
+        body = request.get_json()
+        new_question = body.get('question', None)
+        new_answer = body.get('answer', None)
+        new_category = body.get('category', None)
+        new_difficulty = body.get('difficulty', None)
+        
+        try:
+            
+            question = Question(question=new_question, answer=new_answer, category=new_category, difficulty=new_difficulty)      
+            question.insert()
+            
+            selection = Question.query.order_by(Question.id).all()
+            current_questions = paginate_questions(request, selection)
+            total_questions = len(selection)
+            
+            return jsonify ({
+                'success': True,
+                'created': question.id,
+                'questions': current_questions,
+                'total_questions': total_questions
+            })
+        except:
+            abort(422)
 
     """
     @TODO:
